0e2b8be3da935252e1d99ce9bc3f5a1664a5c7dd
AlphaWhistle <patrickmatuszek@hotmail.com>
2021-10-25 23:52:22 -0400

    added the navbar and allowed switching pages

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import NavBar from '../Components/NavBar'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar></NavBar>
+      <Component {...pageProps} />
+    </>
+    );
+}
+
+export default MyApp;
---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+import NavBar from "../Components/NavBar";
+
+
+const home = () => {
+  return <div>Home</div>;
+};
+
+export default home;
