44ca1a9cb210fa16f629ac75a1a0f7dcd7d3f7f7
AlphaWhistle <patrickmatuszek@hotmail.com>
2021-10-31 23:20:42 -0400

    more

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -1,8 +1,27 @@
 
 import styles from '../styles/navbar.module.css'
+import ProjectCard from '../Components/ProjectCard';
 
 const Projects = () => {
-    return <div className= {styles.bodyText}>Projects</div>
+    const projects = [
+        {
+            title: 'ASTERPOCALYPSE',
+            imgScr: '/',
+            content:
+                'This is a 2d space shooter game, made with python 3 and tkinter. In it you control a ship with your mouse, and shoot the asteroids'
+
+        }
+    ]
+    return <div className= {styles.bodyText}>
+        <h1>My Projects</h1>
+        
+        <p>ASTERPOCALYPSE
+            <br />
+            
+            
+            
+        </p>
+    </div>
 }
 
 export default Projects;
