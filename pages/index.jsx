2870d50656443aba72720c3bc9d23ed1710bf69b
AlphaWhistle <patrickmatuszek@hotmail.com>
2021-11-01 22:39:24 -0400

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import Button from '../Components/Button';
 const home = () => {
   return (
   <div className={styles.home}>
+  <h1>Hi</h1>
   <h1>Hi, I'm Patrick</h1>
   <p>Student, Programmer, Artist</p>
   <Button path= "About">About</Button>
