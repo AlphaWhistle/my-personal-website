bb8ee3e4fe959e3c44ef1b5395cad39854cfaa24
AlphaWhistle <patrickmatuszek@hotmail.com>
2021-11-07 20:21:15 -0500

    more!!!!!!!!!!!!!!

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,16 +1,36 @@
 import styles from '../styles/About.module.css'
+import Image from 'next/image';
 
 const About = () => {
     return <div className={styles.about}>
         <header>
-            <h1>Student Programmer Artist</h1>
+            <div
+                style={{
+                display: "flex",
+                justifyContent: "center",}}
+            >
+                <Image src="/About_Me_pic_1.jpg"width={378} height={351}></Image>
+            </div>
+            <h1>~ Student / Programmer / Artist ~</h1>
             <p>Hi there, I'm Patrick Matuszek
                 <br />
                 <br />
                 Who am I?
                 <br />
                 <br />
-                Currently I am a first year student at University of Toronto Mississauga campus, and am pursuing a degree in Cs</p>
+                Currently I am a first year student at University of Toronto Mississauga campus, and am pursuing a degree in Computer Science
+                <br />
+                <br />
+                In my free time, I do pixel art and animation, which you can check out in the Artwork tab of this Website. As of now I am slowly uploading all my previous projects 
+                and so it may seem a little bare at the moment
+                <br />
+                <br />
+                I am currently designing the art and level design of a 2D puzzle game I plan on creating in the near future, called "Thalssophobia"
+                This is a pretty big project, but updates will be posted on this website under the Projects page.
+                <br />
+                <br />
+                I hope my work interests you
+            </p>
 
         </header>
     </div>;
