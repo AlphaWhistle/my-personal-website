dcffec75809bd2121db9206230ac80da6e6738a8
AlphaWhistle <patrickmatuszek@hotmail.com>
2021-11-07 21:23:06 -0500

    more

--- pages/Artwork.jsx
+++ pages/Artwork.jsx
@@ -9,8 +9,37 @@ const Artwork = () => {
             imgScr: '/Red_Ball_002.gif',
             content:
                 'The Standard intro to animation project',
+            Height: 400,
+            Width: 400,
         },
-        
+
+        {
+            title: 'The Ice Frog',
+            imgScr: '/frog_ice_001.png',
+            content:
+                'A study of frogs, and snow',
+            Height: 400,
+            Width: 400,
+        },
+
+        {
+            title: 'A Flying Eye',
+            imgScr: '/Enemy_001.gif',
+            content:
+                'A potential enemy for a dungeon game',
+            Height: 165,
+            Width: 240,
+        },
+
+        {
+            title: 'A Rocket Ship',
+            imgScr: '/Ship_001.gif',
+            content:
+                'A study of fire, and Rocket Ships',
+            Height: 168,
+            Width: 128,
+        },
+
     ]
     return(
     <div className= {styles.Artwork}>
@@ -18,8 +47,10 @@ const Artwork = () => {
     <br />
     <br />
     {projects.map((project, index) => (
-      <ProjectCard key={index} title={project.title} imgSrc={project.imgScr} content={project.content} />
+      <ProjectCard key={index} title={project.title} imgSrc={project.imgScr} content={project.content} W={project.Width} H={project.Height} />
     ))}
+
+    
     </div>
     );
 };
